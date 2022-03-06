057393472d121963557b05fa0c45c60682d05be0
Athavan Kanapuli <athavankanapuli@gmail.com>
2022-03-06 11:36:07 +0100

    Parse and return all the urls of the html

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,12 +1,12 @@
-import kotlin.text.Regex;
 class HtmlParser {
-
     fun findUrls(html: String): Set<String> {
-       println( A_TAG_PATTERN.findAll(html).first().groups.get(2)?.value)
-        return setOf()
+        return A_TAG_PATTERN.findAll(html)
+            .mapNotNull { it.groups.get(URL_GROUP_INDEX)?.value }
+            .toSet()
     }
 
     companion object {
-         val A_TAG_PATTERN = Regex("<a(?:[^>]*)href=(['\\\"])(http.+?)\\1")
+        val A_TAG_PATTERN = Regex("<a(?:[^>]*)href=(['\\\"])(http.+?)\\1")
+        const val URL_GROUP_INDEX: Int = 2
     }
 }
